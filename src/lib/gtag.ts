bba760cb24df31f8bf8fe5d18ca39b5f76b71ce7
cholis04 <korunkolis04@gmail.com>
2023-09-23 18:27:14 +0700

    fix: Error Type Google Analytics

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,18 +1,18 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const pageview = (url: URL) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag('config', GA_TRACKING_ID as string, {
     page_path: url,
   });
 };
 
 export const event = (
   action: Gtag.EventNames,
-  { category, label, value }: Gtag.EventParams
+  { event_category, event_label, value }: Gtag.EventParams
 ) => {
   window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
+    event_category: event_category,
+    event_label: event_label,
     value: value,
   });
 };
